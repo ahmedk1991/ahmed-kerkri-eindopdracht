f3e972804073685ee03dc605b55c982d051eec63
ahmedk1991 <r0379317@student.thomasmore.be>
2025-01-27 19:47:53 +0100

    first Push

---
+++ src/app/ahmed/page.tsx
@@ -0,0 +1,7 @@
+export default function AhmedPage() {
+    return (
+        <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-green-500 text-white">
+            <h1 className="text-6xl font-bold">Welkom op Ahmed's pagina!</h1>
+        </div>
+    );
+}
