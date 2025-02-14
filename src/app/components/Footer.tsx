bc31e244afc9d0d766432c3d8174589eb25f7e27
ahmedk1991 <ahmed.kerkri@outlook.com>
2025-02-14 14:01:19 +0100

    footer margin aangepast

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,17 +1,17 @@
-import {FaBrain, FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter} from "react-icons/fa";
+import { FaBrain, FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-10">
+        <footer className="bg-gray-800 text-white py-10 mt-auto">
             <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
                 <div>
                     <h3 className="text-lg font-semibold flex items-center space-x-2">
                         <FaBrain />
                         <span>IQ Master</span>
                     </h3>
-                    <p className="text-lg my-2.5">Discover abd unlock your true <br/>
-                        intellectual portential with our certified <br/>
-                        IQ tests</p>
+                    <p className="text-lg my-2.5">Discover and unlock your true <br/>
+                        intellectual potential with our certified <br/>
+                        IQ tests.</p>
                 </div>
                 <div>
                     <h3 className="text-lg font-semibold">Quick links</h3>
@@ -31,17 +31,17 @@ const Footer = () => {
                 </div>
                 <div>
                     <h3 className="text-lg font-semibold">Follow Us</h3>
-                    <ul className="mt-4  flex items-center space-x-2 ">
-                        <li><a href="#" className="hover:underline"><FaTwitter/></a></li>
-                        <li><a href="#" className="hover:underline"><FaFacebook/></a></li>
-                        <li><a href="#" className="hover:underline"><FaInstagram/></a></li>
-                        <li><a href="#" className="hover:underline"><FaLinkedinIn/></a></li>
+                    <ul className="mt-4 flex items-center space-x-4">
+                        <li><a href="#" className="hover:underline"><FaTwitter /></a></li>
+                        <li><a href="#" className="hover:underline"><FaFacebook /></a></li>
+                        <li><a href="#" className="hover:underline"><FaInstagram /></a></li>
+                        <li><a href="#" className="hover:underline"><FaLinkedinIn /></a></li>
                     </ul>
                 </div>
             </div>
-            <p className="text-center m-2">&copy; {new Date().getFullYear()} IQ Master. All rights reserved.</p>
-        </footer>
 
+            <p className="text-center mt-10 py-6">&copy; {new Date().getFullYear()} IQ Master. All rights reserved.</p>
+        </footer>
     );
 };
 
