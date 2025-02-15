4dfe65eae641dfbad17ae8664857fa837fd4e702
ahmedk1991 <ahmed.kerkri@outlook.com>
2025-02-15 13:59:59 +0100

    iq distributie aangepast

--- src/app/components/BellCurve.tsx
+++ src/app/components/BellCurve.tsx
@@ -7,14 +7,27 @@ interface BellCurveProps {
 }
 
 export default function BellCurve({ iqScore }: BellCurveProps) {
+
     const iqDistribution = [
-        { iq: 55, frequency: 5 },
+        { iq: 55, frequency: 2 },
+        { iq: 60, frequency: 4 },
+        { iq: 65, frequency: 8 },
         { iq: 70, frequency: 15 },
-        { iq: 85, frequency: 40 },
-        { iq: 100, frequency: 60 },
-        { iq: 115, frequency: 40 },
+        { iq: 75, frequency: 25 },
+        { iq: 80, frequency: 35 },
+        { iq: 85, frequency: 45 },
+        { iq: 90, frequency: 55 },
+        { iq: 95, frequency: 60 },
+        { iq: 100, frequency: 65 },
+        { iq: 105, frequency: 60 },
+        { iq: 110, frequency: 55 },
+        { iq: 115, frequency: 45 },
+        { iq: 120, frequency: 35 },
+        { iq: 125, frequency: 25 },
         { iq: 130, frequency: 15 },
-        { iq: 145, frequency: 5 },
+        { iq: 135, frequency: 8 },
+        { iq: 140, frequency: 4 },
+        { iq: 145, frequency: 2 },
     ];
 
     return (
