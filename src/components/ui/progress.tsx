a3fb802cdc8e08b77ecfaeeee575213016618a00
ahmedk1991 <r0379317@student.thomasmore.be>
2025-02-17 11:58:30 +0100

    progressbar kleur aangepast

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -17,10 +17,11 @@ const Progress = React.forwardRef<
     )}
     {...props}
   >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
+      <ProgressPrimitive.Indicator
+          className="h-full w-full flex-1 bg-blue-300 transition-all"
+          style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+
   </ProgressPrimitive.Root>
 ))
 Progress.displayName = ProgressPrimitive.Root.displayName
